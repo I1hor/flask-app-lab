ab8d582cfa2520691c91f40070fe75dd5086e554
I1hor <ihor.babiichuk.22@pnu.edu.ua>
2024-10-16 16:00:41 +0300

    copying files from main branch

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World!', 200
+
+@app.route('/homepage') 
+def home():
+    """View for the Home page of your website."""
+
+    return "This is your homepage :) "
+
 
 if __name__ == '__main__':
-    # Використовуйте PORT із змінної оточення
+    
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
